7d6655cdd4c291bfa0aecc27e8c3c90c35e8677b
micha-s <59810949+micha-s@users.noreply.github.com>
2022-07-10 23:06:18 +0200

    fix

--- startup.sh
+++ startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "nginx configuration webroot has to be laravels public folder"
-sed -i -e 's/root \/root\/site\/wwwroot;/root \/root\/site\/wwwroot\/public;/g' /etc/nginx/sites-available/default
+sed -i -e 's/root \/home\/site\/wwwroot;/root \/home\/site\/wwwroot\/public;/g' /etc/nginx/sites-available/default
  
 echo "Reloading nginx to apply new configuration"
 service nginx reload
