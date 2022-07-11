46de3318b765692eed88bdf61c2a1c176932b1c3
micha-s <59810949+micha-s@users.noreply.github.com>
2022-07-11 11:22:58 +0200

    key

--- startup.sh
+++ startup.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 echo "nginx configuration webroot has to be laravels public folder"
 sed -i -e 's/root \/home\/site\/wwwroot;/root \/home\/site\/wwwroot\/public;/g' /etc/nginx/sites-enabled/default
- 
+
+echo "generating new application key"
+php artisan key:generate
+
 echo "Reloading nginx to apply new configuration"
 service nginx reload
- 
+
 # needed first time deploy:
 # cd /home/site/wwwroot && cp -n .env.example .env 
-# php artisan key:generate
